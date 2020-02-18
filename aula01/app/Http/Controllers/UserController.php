f6277fce34c9a406713c21a52970953cdc2822ef
JdC <jdc_santos@outlook.com>
2020-02-18 01:47:45 +0000

    criando o primeiro controller

---
+++ aula01/app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function verUsuario(){
+        // echo apenas para testar
+        echo "<h1>Hello World</h1>";
+    }
+}
