bd8bab7edd75273e023b67672e2d6f208bc04a8d
João Vitor Novais <joaoeuno2011@gmail.com>
2020-03-11 01:31:21 +0000

    adicionando lista de usuarios

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('users');
 });
+
+Route::get('usuarios', 'UserController@users')->name('users');
+
+Route::get('cadastrar', function () {
+    return view('novo');
+});
+
+Route::get('editar/{userId}', 'UserController@edit');
+
+Route::post('storage', 'UserController@storage');
+
+Route::put('update/{user}', 'UserController@update');
+
+Route::delete('delete/{user}', 'UserController@delete');
